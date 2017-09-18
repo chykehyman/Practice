16494cf3296a437d83cbffe3ce2c04f1a7f8dda8
chykehyman <hyginuschinwoke@gmail.com>
2017-09-18 12:25:10 +0100

    implememted my node web server

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './node_modules/jquery/dist')));
+app.use(express.static(__dirname + './public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.sendFile(path.join(__dirname, './material design/index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
